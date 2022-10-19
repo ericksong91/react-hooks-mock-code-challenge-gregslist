6ab5cbbc92da50d8368a9f4225e3fb292b9eb65f
Eric Song <ericksong91@gmail.com>
2022-10-19 16:36:07 -0400

    commit tree b5a4e049235b13a4960598f3cfde683146cc6d51

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(()=>{fetchListings()}, [])
+
+  function fetchListings () {
+    const url = "http://localhost:6001/listings";
+
+    fetch(url)
+    .then((r)=>r.json())
+    .then((data)=>setListings(data))
+  }
+
+  function handleDeleteItem(delItem){
+    console.log("In Container:", delItem)
+    const updatedItems = listings.filter((item)=> {
+      return item.id !== delItem
+    })
+
+    return setListings(updatedItems)
+  }
+
+  const updatedListing = listings.map((listItem)=>{
+    return <ListingCard key={listItem.id} item={listItem} onDelete={handleDeleteItem} />
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {updatedListing}
       </ul>
     </main>
   );
